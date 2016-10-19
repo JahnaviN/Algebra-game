93d93d1404fd78cee36eec4c3ff60563d58d710c
Jahnavi Nukireddy <nukireddy.jahnavi@students.iiit.ac.in>
2016-10-19 16:04:40 +0530

    drag and drop script

---
+++ Algebra game/Assets/script2.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class script2 : MonoBehaviour, IDropHandler
+{
+
+    public GameObject item
+    {
+        get
+        {
+            if (transform.childCount > 0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+            return null;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        throw new NotImplementedException();
+    }
+}
