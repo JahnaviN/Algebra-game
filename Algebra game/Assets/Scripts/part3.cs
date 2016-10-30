398f18f5309d19d4ea45bbcf15662613ab681880
Jahnavi Nukireddy <nukireddy.jahnavi@students.iiit.ac.in>
2016-10-30 23:00:58 +0530

    part-3

---
+++ Algebra game/Assets/Scripts/part3.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class part3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        for(int i=0; i<4; i++)
+        {
+            Image img = transform.GetChild(i).GetChild(0).GetComponent<Image>();
+            var obj = Resources.Load("a") as Texture2D;
+            Rect rec = new Rect(0, 0, obj.width, obj.height);
+            var x = Sprite.Create(obj, rec, new Vector2(0.5f, 0.5f));
+            img.sprite = x;
+        }
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+}
--- Algebra game/Assets/Scripts/script2.cs
+++ Algebra game/Assets/Scripts/script2.cs
@@ -6,39 +6,9 @@ using UnityEngine.UI;
 
 public class script2 : MonoBehaviour, IDropHandler
 {
-    /*public GameObject item
-    {
-        get
-        {
-            if (transform.childCount > 0)
-            {
-                return transform.GetChild(0).gameObject;
-            }
-            return null;
-        }
-    }
 
     public void OnDrop(PointerEventData eventData)
     {
-        if (!item)
-        {
-            // Transform prevParent = script1.itemBeingDragged.transform.parent;
-            script1.itemBeingDragged.transform.SetParent(transform);
-            script1.itemBeingDragged.transform.position = transform.position;
-        }
-    }*/
-
-
-    public void OnDrop(PointerEventData eventData)
-    {
-        /*if (!item)
-        {
-            Debug.Log(item.ToString());
-            item = script1.itemBeingDragged.transform.GetComponent<Image>().sprite;
-            //script1.itemBeingDragged.transform.SetParent(transform);
-            //script1.itemBeingDragged.transform.position = transform.position;
-        }*/
-
         Image img = transform.GetChild(0).GetComponent<Image>();
         img.sprite = script1.itemBeingDragged.transform.GetComponent<Image>().sprite;
         script1.itemBeingDragged.transform.position = script1.itemBeingDragged.transform.parent.position;
